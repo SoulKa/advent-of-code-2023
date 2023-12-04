75fc7919d4dfbc1ce4ec4cb3efa524262dab468c
Wilke, Louis <louis.wilke@exxeta.com>
2023-12-04 22:17:32 +0100

    finished day 4

--- 04/index.ts
+++ 04/index.ts
@@ -1,1 +1,28 @@
-export default async function run(input: string) {}
+export default async function run(input: string) {
+  let sum = 0;
+
+  for (const line of input.split("\n")) {
+    const [winningString, numString] = line.split(":")[1].split("|");
+    const winningSet = new Set(
+      winningString
+        .split(" ")
+        .filter((s) => s !== "")
+        .map((s) => Number.parseInt(s))
+    );
+    const numSet = new Set(
+      numString
+        .split(" ")
+        .filter((s) => s !== "")
+        .map((s) => Number.parseInt(s))
+    );
+
+    const wonSet = intersect(winningSet, numSet);
+    if (wonSet.size !== 0) sum += 1 << (wonSet.size - 1);
+  }
+
+  return sum;
+}
+
+function intersect<T>(a: Set<T>, b: Set<T>) {
+  return new Set([...a].filter((av) => b.has(av)));
+}
