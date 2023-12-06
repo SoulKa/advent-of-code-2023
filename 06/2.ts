ad569a1ca944eb143f868fa0791d591e040a5062
SoulKa <louiswilke@gmx.de>
2023-12-06 09:02:09 +0100

    using quadratic formula

--- 06/2.ts
+++ 06/2.ts
@@ -1,18 +1,15 @@
-// DISCLAIMER: I AM SORRY FOR THE BRUTE FORCE METHOD :D
-
 export default async function run(input: string) {
   const [timeString, distanceString] = input.split("\n");
   const timeLimit = Number.parseInt(timeString.split(":")[1].replace(/\s/g, ""));
   const record = Number.parseInt(distanceString.split(":")[1].replace(/\s/g, ""));
 
-  let winCounts = 0;
-  for (let tButton = 1; tButton < timeLimit; tButton++) {
-    const distance = getDistanceForButtonPress(tButton, timeLimit);
-    if (distance > record) winCounts++;
-  }
-  return winCounts;
+  // get the lower and upper threshold for pressing the button to win (intersection with record)
+  const intersections = getCurveIntersections(record, timeLimit);
+  const [lowerLimit, upperLimit] = intersections.map((n) => Math.ceil(n)).sort();
+  return upperLimit - lowerLimit;
 }
 
-function getDistanceForButtonPress(holdTime: number, raceTime: number) {
-  return (raceTime - holdTime) * holdTime;
+function getCurveIntersections(c: number, minusB: number) {
+  const root = Math.sqrt(minusB * minusB - 4 * c);
+  return [(minusB + root) / 2, (minusB - root) / 2]; // use quadratic formula
 }
