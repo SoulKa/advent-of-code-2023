2dee8eb58fb62b72be63e01c7a948b1d362cc375
SoulKa <louiswilke@gmx.de>
2023-12-07 09:36:53 +0100

    finished 7.1

--- 07/1.ts
+++ 07/1.ts
@@ -1,10 +1,16 @@
+import { inspect } from "util";
+
 declare type Card = "A" | "K" | "Q" | "J" | "T" | "9" | "8" | "7" | "6" | "5" | "4" | "3" | "2";
 
 declare type Range = Card[];
 declare type Cut = [range: Range, remainder: Card[]];
 
 //declare type Play = [primary: Range[], secondary: Range];
-declare type Play = [primary: Range[], secondary: Range];
+declare type Play = {
+  primary: Range[];
+  secondary: Range;
+  kind: Kind;
+};
 enum Kind {
   HIGH_CARD = 0,
   PAIR = 1,
@@ -40,14 +46,12 @@ export default async function run(input: string) {
     const bid = Number.parseInt(bidString);
     const cards = cardsString.split("") as Card[];
     const hand = new Hand(cards);
-    return {
-      play: getHighestPlay(hand),
-      bid,
-    };
+    return Object.assign(getHighestPlay(hand), { bid });
   });
 
   // sort by highest kinds of plays (ascending)
-  plays.sort((a, b) => a.play.kind - b.play.kind);
+  plays.sort((a, b) => comparePlays(a.play, b.play));
+  console.log(inspect(plays.slice(plays.length - 100), true, null, true));
   return plays.map(({ bid }, i) => bid * (i + 1)).reduce((sum, v) => sum + v, 0);
 }
 
@@ -71,27 +75,36 @@ function getPlay(hand: Hand, kind: Kind): Play | undefined {
     case Kind.FOUR_OF_A_KIND:
     case Kind.FIVE_OF_A_KIND:
       for (const [range, secondary] of hand.getCuts(PAIR_SIZE_MAP[kind])) {
-        if (allCardsEqual(range)) return [[range], secondary];
+        if (allCardsEqual(range)) return { primary: [range], secondary, kind };
       }
       break;
     case Kind.TWO_PAIR:
+      // find all pairs
       let pairs = [] as Range[];
-      let cuts = hand.getCuts(2);
-      for (let i = 0; i < cuts.length; i++) {
-        const [range, remainder] = cuts[i];
-        if (allCardsEqual(range)) {
-          pairs.push(range);
-          cuts = new Hand(remainder).getCuts(2);
-        }
+      let play: Play | undefined;
+      do {
+        play = getPlay(hand, Kind.PAIR);
+        if (play == null) break;
+        pairs.push(play.primary[0]);
+        hand = new Hand(play.secondary);
+      } while (true);
+
+      // if two pairs ==> return
+      if (pairs.length === 2) {
+        return {
+          primary: pairs,
+          secondary: hand.cards,
+          kind,
+        };
       }
       break;
     case Kind.FULL_HOUSE:
       const highCut = hand.getCut(0, 3);
       const lowCut = hand.getCut(0, 2);
       if (allCardsEqual(highCut[0]) && allCardsEqual(highCut[1])) {
-        return cutToPlay(highCut);
+        return cutToPlay(highCut, kind);
       } else if (allCardsEqual(lowCut[0]) && allCardsEqual(lowCut[1])) {
-        return cutToPlay([lowCut[1], lowCut[0]]);
+        return cutToPlay([lowCut[1], lowCut[0]], kind);
       }
       break;
     default:
@@ -110,7 +123,25 @@ function compareCards(a: Card, b: Card) {
  * Weakest plays will come first!
  */
 function comparePlays(a: Play, b: Play) {
-  a - b.kind;
+  // compare kind
+  if (a.kind !== b.kind) return a.kind - b.kind;
+  let comp: number;
+
+  // compare primary
+  comp = compareCards(b.primary[0][0], a.primary[0][0]);
+  if (comp !== 0) return comp;
+  if (b.primary.length === 2) {
+    comp = compareCards(b.primary[1][0], a.primary[1][0]);
+    if (comp !== 0) return comp;
+  }
+
+  // compare secondary
+  if (a.secondary.length !== b.secondary.length) throw new RangeError(`Secondary sizes do not match`);
+  for (let i = 0; i < a.secondary.length; i++) {
+    comp = compareCards(b.secondary[i], a.secondary[i]);
+    if (comp !== 0) return comp;
+  }
+  return 0;
 }
 
 function allCardsEqual(range: Range) {
@@ -118,8 +149,12 @@ function allCardsEqual(range: Range) {
   return !range.some((card) => card !== targetCard);
 }
 
-function cutToPlay(range: Cut): Play {
-  return [[range[0]], range[1]];
+function cutToPlay(range: Cut, kind: Kind): Play {
+  return {
+    primary: [range[0]],
+    secondary: range[1],
+    kind,
+  };
 }
 
 class Hand {
