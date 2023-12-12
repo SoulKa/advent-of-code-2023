a91f89c2300ac6234559575689d232584b9d1c9d
SoulKa <louiswilke@gmx.de>
2023-12-12 21:33:35 +0100

    with code polishing

--- 12/2.ts
+++ 12/2.ts
@@ -8,8 +8,6 @@ declare type GetNumArrangements = (
   conditionOverride?: Condition
 ) => number;
 
-const CACHE = new Map<string, number>();
-
 export default async function run(input: string) {
   const tasks = input
     .split("\n")
@@ -25,59 +23,75 @@ export default async function run(input: string) {
 
   let sum = 0;
   for (const { arrangement, groups } of tasks) {
-    function getNumArrangements(
+    const getNumArrangements = withCache(function (
       arrangementIndex = 0,
       currentGroup = 0,
       groupIndex = 0,
       conditionOverride?: Condition
     ): number {
-      const cacheKey = [arrangementIndex, currentGroup, groupIndex, conditionOverride].join(",");
-      const cached = CACHE.get(cacheKey);
-      if (cached !== undefined) return cached;
-
       if (arrangementIndex === arrangement.length) {
-        return setCache(cacheKey, currentGroup === 0 && groupIndex === groups.length ? 1 : 0);
+        return currentGroup === 0 && groupIndex === groups.length ? 1 : 0;
       }
       if (groupIndex === groups.length && currentGroup !== 0) {
         //console.log("too many groups", arrangement, currentGroup, groupIndex);
-        return setCache(cacheKey, 0);
+        return 0;
       }
       if (currentGroup > groups[groupIndex]) {
         //console.log("too many in group", arrangement, currentGroup, groupIndex);
-        return setCache(cacheKey, 0);
+        return 0;
       }
 
       const condition = conditionOverride || arrangement[arrangementIndex];
       const targetGroup = groups[groupIndex];
       switch (condition) {
         case "?":
-          return setCache(
-            cacheKey,
+          return (
             getNumArrangements(arrangementIndex, currentGroup, groupIndex, ".") +
-              getNumArrangements(arrangementIndex, currentGroup, groupIndex, "#")
+            getNumArrangements(arrangementIndex, currentGroup, groupIndex, "#")
           );
         case "#":
-          return setCache(cacheKey, getNumArrangements(arrangementIndex + 1, currentGroup + 1, groupIndex));
+          return getNumArrangements(arrangementIndex + 1, currentGroup + 1, groupIndex);
         case ".":
           if (currentGroup === 0) {
-            return setCache(cacheKey, getNumArrangements(arrangementIndex + 1, 0, groupIndex));
+            return getNumArrangements(arrangementIndex + 1, 0, groupIndex);
           } else if (currentGroup === targetGroup) {
-            return setCache(cacheKey, getNumArrangements(arrangementIndex + 1, 0, groupIndex + 1));
+            return getNumArrangements(arrangementIndex + 1, 0, groupIndex + 1);
           } else {
-            return setCache(cacheKey, 0);
+            return 0;
           }
       }
-    }
-    CACHE.clear();
+    });
 
-    const numArrangements = getNumArrangements();
-    console.log(numArrangements);
-    sum += numArrangements;
+    sum += getNumArrangements();
   }
   return sum;
 }
 
-function setCache(key: string, value: number) {
-  CACHE.set(key, value);
-  return value;
+function withCache<T extends (...args: any[]) => any>(fn: T): T {
+  const cache = new Map<string, ReturnType<T>>();
+  return function (...args: Parameters<T>): ReturnType<T> {
+    // Check if the result is already cached
+    const cacheKey = generateCacheKey(...args);
+    const cached = cache.get(cacheKey);
+    if (cached !== undefined) return cached;
+
+    // Not cached, call the original function
+    const result = fn(...args);
+    cache.set(cacheKey, result);
+    return result;
+  } as T;
+}
+
+function generateCacheKey<T extends (...args: any[]) => any>(...args: Parameters<T>) {
+  const cacheKey = [] as any[];
+  for (const arg of args) {
+    if (Array.isArray(arg)) {
+      cacheKey.push(...arg);
+    } else if (typeof arg === "object") {
+      cacheKey.push(JSON.stringify(arg));
+    } else {
+      cacheKey.push(arg);
+    }
+  }
+  return cacheKey.join(",");
 }
