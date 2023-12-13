ce9c606609e27606a8d73bd2116bac41fd364c9c
SoulKa <louiswilke@gmx.de>
2023-12-13 19:14:53 +0100

    add cmd line args

--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
 import fs from "fs";
 import path from "path";
 
+type OptionArg = "in";
 type RunFunc = (input: string) => Promise<string | number | undefined>;
-const dayArg = process.argv[2];
+let dayArg: number | undefined;
+let inputFileName = "input.txt";
+
+// parse arguments
+for (let arg of process.argv.slice(2)) {
+  if (!arg.startsWith("--")) {
+    dayArg = Number.parseInt(arg);
+    continue;
+  }
+
+  const [key, value] = arg.slice(2).split("=");
+  console.log(key, value);
+  switch (key as OptionArg) {
+    case "in":
+      inputFileName = value || "test-input.txt";
+      break;
+    default:
+      throw new Error(`Unknown argument: --${arg}`);
+  }
+}
 
 async function main() {
   console.log("Advent of Code 2023\n");
@@ -10,10 +30,17 @@ async function main() {
   // iterate over all riddles
   for (const dir of fs.readdirSync(__dirname, { withFileTypes: true })) {
     const { name } = dir;
-    if (dir.isDirectory() && /^[0-9]{2}(\.[0-9]+)?$/.test(name) && (dayArg == null || name === dayArg)) {
+    if (
+      dir.isDirectory() &&
+      /^[0-9]{2}(\.[0-9]+)?$/.test(name) &&
+      (dayArg == null || Number.parseInt(name) === dayArg)
+    ) {
       const dirPath = path.join(__dirname, name);
-      const inputPath = path.join(dirPath, "input.txt");
-      if (!fs.existsSync(inputPath)) continue;
+      const inputPath = path.join(dirPath, inputFileName);
+      if (!fs.existsSync(inputPath)) {
+        console.log(`Skipping day ${name} because input is missing`);
+        continue;
+      }
       console.log(`Day ${name}:`);
 
       // read input
